14610acdabea32387466e7f8bb006f57420fb9db
Tan <khee.houy.tan@accenture.com>
2022-04-26 16:34:54 +0800

    UPD: Update CSS for mouse hover and fix text overflowing.

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 
 function AddNote(props) {
-    const maxLength = 200;
+    const maxLengthTitle = 50;
+    const maxLengthContent = 200;
     const [isDisplay, setIsDisplay] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
     const [note, setNote] = useState({
@@ -21,7 +22,7 @@ function AddNote(props) {
 
     const handleKeyUp = (e) => {
         const counterElement = document.getElementById("lengthCounter")
-        counterElement.innerHTML = maxLength - e.target.value.length;
+        counterElement.innerHTML = maxLengthContent - e.target.value.length;
 
         if (e.target.value.length > 0) {
             setIsDisplay(true);
@@ -33,9 +34,9 @@ function AddNote(props) {
     return (
         <div>
             <form>
-                {isExpanded && <input name="title" placeholder="Title (Optional)" onChange={handleChange} value={note.title} />}
-                <textarea name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} onClick={expand} onChange={handleChange} onKeyUp={handleKeyUp} value={note.content} maxLength={maxLength} />
-                {isExpanded && <p>Characters left: <span id="lengthCounter">{maxLength}</span></p>}
+                {isExpanded && <input name="title" placeholder="Title (Optional)" onChange={handleChange} value={note.title} maxLength={maxLengthTitle} />}
+                <textarea name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} onClick={expand} onChange={handleChange} onKeyUp={handleKeyUp} value={note.content} maxLength={maxLengthContent} />
+                {isExpanded && <p>Characters left: <span id="lengthCounter">{maxLengthContent}</span></p>}
                 {isDisplay && <button onClick={(e) => {
                     e.preventDefault();
                     props.onAdd(note);
@@ -44,6 +45,7 @@ function AddNote(props) {
                         content: ""
                     })
                     setIsExpanded(false);
+                    setIsDisplay(false);
                 }}><AddIcon /></button>}
             </form>
         </div>
