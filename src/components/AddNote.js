9ed39dadd77c95abec9dec0d5b850a03a2634c57
Tan <khee.houy.tan@accenture.com>
2022-04-26 15:49:13 +0800

    UPD: Update AddNote component to have a function to count the number of characters left.

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 
 function AddNote(props) {
+    const maxLength = 200;
+    const [isDisplay, setIsDisplay] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
     const [note, setNote] = useState({
         title: "",
@@ -17,12 +19,24 @@ function AddNote(props) {
         setIsExpanded(true);
     }
 
+    const handleKeyUp = (e) => {
+        const counterElement = document.getElementById("lengthCounter")
+        counterElement.innerHTML = maxLength - e.target.value.length;
+
+        if (e.target.value.length > 0) {
+            setIsDisplay(true);
+        } else {
+            setIsDisplay(false);
+        }
+    }
+
     return (
         <div>
             <form>
-                {isExpanded && <input name="title" placeholder="Title" onChange={handleChange} value={note.title} />}
-                <textarea name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} onClick={expand} onChange={handleChange} value={note.content} />
-                {isExpanded && <button onClick={(e) => {
+                {isExpanded && <input name="title" placeholder="Title (Optional)" onChange={handleChange} value={note.title} />}
+                <textarea name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} onClick={expand} onChange={handleChange} onKeyUp={handleKeyUp} value={note.content} maxLength={maxLength} />
+                {isExpanded && <p>Characters left: <span id="lengthCounter">{maxLength}</span></p>}
+                {isDisplay && <button onClick={(e) => {
                     e.preventDefault();
                     props.onAdd(note);
                     setNote({
