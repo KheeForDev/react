65618d43b5cf72eeac0838cbe5490052bf36de7f
Tan <khee.houy.tan@accenture.com>
2022-04-26 13:10:16 +0800

    UPD: Add linkedin and github icon and update style for footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
         <footer>
-            <p>Copyright &copy; {currentYear}</p>
-        </footer>
+            <p>Copyright &copy; {currentYear} Khee Houy, Tan</p>
+            <p>
+                <a href="https://www.linkedin.com/in/kheehouytan" target="_blank" rel="noreferrer" style={{ color: "inherit" }}><LinkedInIcon /></a>
+                <a href="https://github.com/KheeForDev/react/tree/note-keeper-frontend" target="_blank" rel="noreferrer" style={{ color: "inherit" }}><GitHubIcon /></a>
+            </p>
+        </footer >
     );
 }
 
