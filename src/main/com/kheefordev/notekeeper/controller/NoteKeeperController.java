32287fe7433991d8d72659366ce45c65ceaca60f
Tan <khee.houy.tan@accenture.com>
2022-05-01 14:25:34 +0800

    UPD: Allow controller to enables cross-origin resource sharing.

--- src/main/com/kheefordev/notekeeper/controller/NoteKeeperController.java
+++ src/main/com/kheefordev/notekeeper/controller/NoteKeeperController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kheefordev.notekeeper.model.Note;
 import com.kheefordev.notekeeper.service.NoteService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/note")
 public class NoteKeeperController {
