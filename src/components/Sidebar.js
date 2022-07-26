a7b44b73e1ee0f7d9439b7a5a877bfd9eb7cda83
Tan <khee.houy.tan@accenture.com>
2022-07-26 18:04:32 +0800

    UPD: sidebar css.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { UilExclamationCircle, UilShieldCheck, UilSetting, UilSignout } from '@iconscout/react-unicons'
 
 import useAuth from "../hook/useAuth";
+import img_placeholder from "../asserts/images/profile_placeholder.jpg";
 
 const Sidebar = () => {
     const { auth, setAuth } = useAuth();
@@ -14,11 +16,14 @@ const Sidebar = () => {
 
     return (
         <nav className="sidebar">
+            {/* <div>
+                <img src={img_placeholder} />
+            </div> */}
             <ul>
-                <li><Link to="/">About</Link></li>
-                {auth?.roles?.includes("ROLE_USER") && <li><Link to="/warranty">Warranty</Link></li>}
-                {auth?.roles?.includes("ROLE_ADMIN") && <li><Link to="/setting">Setting</Link></li>}
-                <li><Link to="/login" onClick={logout}>Logout</Link></li>
+                <li><Link to="/"><UilExclamationCircle />About</Link></li>
+                {auth?.roles?.includes("ROLE_USER") && <li><Link to="/warranty"><UilShieldCheck />Warranty</Link></li>}
+                {auth?.roles?.includes("ROLE_ADMIN") && <li><Link to="/setting"><UilSetting />Setting</Link></li>}
+                <li><Link to="/login" onClick={logout}><UilSignout />Logout</Link></li>
             </ul>
         </nav>
     );
