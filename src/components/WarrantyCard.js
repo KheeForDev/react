76ccf2693b77006f5a29fd71683c83768be2e438
Khee Houy, Tan <kheefordev@gmail.com>
2022-08-03 23:12:54 +0800

    UPD: Destructure props.

--- src/components/WarrantyCard.js
+++ src/components/WarrantyCard.js
@@ -3,13 +3,13 @@ import Card from 'react-bootstrap/Card';
 
 import img_placeholder from "../asserts/images/286x180.jpg";
 
-const WarrantyCard = (props) => {
+const WarrantyCard = ({id, productName, brand}) => {
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={img_placeholder} />
             <Card.Body>
-                <Card.Title>{props.productName}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{props.brand}</Card.Subtitle>
+                <Card.Title>{productName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{brand}</Card.Subtitle>
                 <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
