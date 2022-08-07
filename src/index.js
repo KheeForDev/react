bab967fa7216a51fa9b0d146038652a3f7af1be4
Khee Houy, Tan <kheefordev@gmail.com>
2022-08-07 09:34:07 +0800

    UPD: Disable strict mode.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <Routes>
@@ -33,5 +33,5 @@ root.render(
         />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
